a2150fb931f86175bd2deee5ea450d1604df9c1c
Mareedu Saibabu <a3097640@bestbuy.com>
2025-07-02 19:19:44 +0530

    Auth

--- micros/auth/src/main.ts
+++ micros/auth/src/main.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import indexAuthRouter from './routes';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(express.json());
+
+// Authentication routes
+app.use('/api/v1', indexAuthRouter);
+
+// Health check route
+app.get('/health', (req, res) => {
+  res.send({ message: 'Hello from Auth API' });
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ 🚀 ready 🚀 ] http://${host}:${port}`);
 });
